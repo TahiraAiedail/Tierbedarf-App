79ec972dd755ad06f09c72bd09f295647c0e82a7
lschwiderski <91699587+lschwiderski@users.noreply.github.com>
2023-06-11 13:01:53 +0200

    Umbennenung
    alt = sign-up ; neu = registrierung

---
+++ src/app/anmeldung/registrierung/registrierung.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrierung',
+  templateUrl: './registrierung.component.html',
+  styleUrls: ['./registrierung.component.css']
+})
+export class registrierungComponent {
+
+}
