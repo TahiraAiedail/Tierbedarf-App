49fad3ac8a1c7a89da91ab53ccc1d991a9b7a979
jreddig04 <105049594+jreddig04@users.noreply.github.com>
2023-06-14 15:50:13 +0200

    anfang

---
+++ server.js
@@ -0,0 +1,22 @@
+app.get('/pruefungen/',function(req,res)
+{
+    var con = mysql.createConnection({
+        database: "23_IT_Gruppe1" ,
+        host: "195.37.176.178",
+        port: "20133",
+        user: "23_IT_Grp_1",
+        password: ")JrYRPeg1NSk45A0yi1H",
+    });
+
+    con.connect(function(err) {
+        if(err) throw err;
+        console.log("Connected");
+
+
+    })
+}
+
+
+
+
+)
