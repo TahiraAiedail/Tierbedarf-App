5e35b093223e4e2ce92f70b78eea38425c9dde28
malumeau <105048993+malumeau@users.noreply.github.com>
2023-07-01 00:52:50 +0200

    Persistence (db funzt aber noch nicht)

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-app.get('/pruefungen/',function(req,res)
-{
-    var con = mysql.createConnection({
-        database: "23_IT_Gruppe1" ,
-        host: "195.37.176.178",
-        port: "20133",
-        user: "23_IT_Grp_1",
-        password: ")JrYRPeg1NSk45A0yi1H",
+const express = require('express');
+const app = express();
+const port = 4200;
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+
+// Middleware to parse JSON entities.
+app.use(bodyParser.json());
+
+// Middleware to parse urlencoded bodies.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    let db = new sqlite3.Database('./Datenbank.db', (err) => { 
+        if (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+            return;
+        }
+        console.log('Connected to the SQLite database.');
     });
 
-}
-)
+    let sql = 'SELECT Name, Tierart FROM Tierheimtiere';
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+            return;
+        }
+        res.json(rows);
+    });
+
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        console.log('Closed the database connection.');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
