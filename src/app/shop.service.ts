eebf3dbcdd115328e3f289a1a347a32b37b54202
lschwiderski <91699587+lschwiderski@users.noreply.github.com>
2023-07-12 08:24:03 +0200

    Bestellübersicht
    für Präsentationszwecke

--- src/app/shop.service.ts
+++ src/app/shop.service.ts
@@ -13,15 +13,15 @@ export class ShopService {
   }
 
   createBestellung(bestellung: any) {
-    const date = new Date();
+    /*const date = new Date();
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const day = date.getDate().toString().padStart(2, '0');
   
     const formattedDate = `${year}-${month}-${day}`;
-  
+  */
     const requestBody = {
-      Datum: formattedDate,
+      //Datum: formattedDate,
       BestellartID: bestellung.bestellartID,
       MitarbeiterID: bestellung.mitarbeiterID,
       KundenID: bestellung.kundenID,
